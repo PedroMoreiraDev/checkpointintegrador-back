00f59ab22ca11acfbe5b4e754c738a918465febf
Duyllyan <duyllyan@gmail.com>
2021-12-01 10:25:10 -0300

    Projeto criado

---
+++ src/main/java/br/com/turma04/ctdcommerce/CtdCommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.turma04.ctdcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtdCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtdCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/turma04/ctdcommerce/CtdCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.turma04.ctdcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtdCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
