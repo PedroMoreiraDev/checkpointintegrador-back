bf1c9da13a5d8f9d3be23da0c4c3ee65f000919b
Duyllyan <duyllyan@gmail.com>
2021-12-02 11:56:16 -0300

    inseridas queries de teste

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO category (name) VALUES ('food'), ('drink'), ('eletronic'), ('cloathing'),('jewelery'),('toys');
+INSERT INTO product (title, price, description, image, category_id) values
+    ('Mouse', 20, 'Mouse Wireless Optical Mice with USB Nano Receiver, 2.4G', 'https://m.media-amazon.com/images/I/41VpUwvy5OL._AC_SL1009_.jpg', 3),
+    ('Keyboard', 30, 'Wireless Keyboard', 'https://snpi.dell.com/snp/images2/300/pt-br~AB284455/AB284455.jpg', 3),
+    ('tshirt', 50, 'Lacoste', 'https://image1.lacoste.com/dw/image/v2/AAQM_PRD/on/demandware.static/Sites-BRECOM-Site/Sites-master/pt_BR/dwc0d815f0/TH7618_166_20.jpg?imwidth=915&impolicy=product', 4),
+    ('Jade Necklace', 200, 'Necklace with jade stones', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.aliexpress.com%2Fitem%2F4000703354075.html&psig=AOvVaw1tLsW27gbeyAVc2jCq_M_2&ust=1638541436004000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOC-w9WoxfQCFQAAAAAdAAAAABAD', 5),
+    ('Buzz Lightyear', 30, 'Buzz toy doll', 'https://m.media-amazon.com/images/I/510U18HH83L._AC_SX425_.jpg', 6),
+    ('Wine Cantinho do Vale', 10, 'A very tasty and light wine', 'https://www.imigrantesbebidas.com.br/bebida/images/products/full/81345_Vinho_Cantinho_Do_Vale_4L.1534622162.jpg', 2);
+
+
+
+
+
+
